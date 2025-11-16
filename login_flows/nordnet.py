1cf222929c169e5b1962a6d21bb5fef264c8fe4f
Lukas Hundt Petersen <lukashundtpetersen@gmail.com>
2025-11-16 16:13:20 +0100

    nordnet: Add error handling and cleanup code courtesy of @Calo-coder

--- login_flows/nordnet.py
+++ login_flows/nordnet.py
@@ -1,95 +1,156 @@
 # Script for https://www.nordnet.dk/logind
-import requests, json, base64, sys, string, secrets, uuid, re
+import requests, json, base64, sys, string, secrets, uuid
 from urllib.parse import urlparse, parse_qs
 from bs4 import BeautifulSoup
 sys.path.append("..")
 sys.path.append(".")
 from BrowserClient.Helpers import get_authentication_code, process_args, get_default_args
-from datetime import datetime, date
 
-argparser = get_default_args()
-args = argparser.parse_args()
+def nordnet_login(user_id, password='', method="APP", proxy=None):
+    nordnet_session = requests.Session()
+    if proxy:
+        nordnet_session.proxies.update({"http": f"socks5://{proxy}", "https": f"socks5://{proxy}" })
 
-method, user_id, password, proxy = process_args(args)
-session = requests.Session()
-if proxy:
-    session.proxies.update({"http": f"socks5://{proxy}", "https": f"socks5://{proxy}" })
+    # First part of Nordnet procedure
+    nem_login_state = uuid.uuid4()
+    digits = string.digits
+    form_digits = ''.join(secrets.choice(digits) for i in range(29))
 
-# First part of Nordnet procedure
-nem_login_state = uuid.uuid4()
-digits = string.digits
-form_digits = ''.join(secrets.choice(digits) for i in range(29))
+    login_url = f"https://id.signicat.com/oidc/authorize?client_id=prod.nordnet.dk.8x&response_type=code&redirect_uri=https://www.nordnet.dk/login&scope=openid signicat.national_id&acr_values=urn:signicat:oidc:method:mitid-cpr&state=NEXT_OIDC_STATE_{nem_login_state}"
 
-login_url = f"https://id.signicat.com/oidc/authorize?client_id=prod.nordnet.dk.8x&response_type=code&redirect_uri=https://www.nordnet.dk/login&scope=openid signicat.national_id&acr_values=urn:signicat:oidc:method:mitid-cpr&state=NEXT_OIDC_STATE_{nem_login_state}"
+    request = nordnet_session.get(login_url)
+    if request.status_code != 200:
+        msg = f"\n  Failed Nordnet session.get attempt:\n"\
+              f"    Status code: {request.status_code}\n"\
+              f"    Reason: {request.reason}\n"\
+              f"    Content: {request.content}"
+        raise Exception(msg)
 
-request = session.get(login_url)
-if request.status_code != 200:
-    print(f"Failed session setup attempt, status code {request.status_code}")
-    raise Exception(request.content)
+    soup = BeautifulSoup(request.text, 'lxml')
+    next = soup.div['data-index-url']
 
-soup = BeautifulSoup(request.text, 'lxml')
-next = soup.div['data-index-url']
-request = session.get(next)
-soup = BeautifulSoup(request.text, 'lxml')
+    request = nordnet_session.get(next)
+    if request.status_code != 200:
+        msg = f"\n  Failed Nordnet session.get attempt:\n"\
+              f"    Status code: {request.status_code}\n"\
+              f"    Reason: {request.reason}\n"\
+              f"    Content: {request.content}"
+        raise Exception(msg)
 
-request = session.post(soup.div.next['data-base-url']+soup.div.next['data-init-auth-path'])
-if request.status_code != 200:
-    print(f"Failed session setup attempt, status code {request.status_code}")
-    raise Exception(request.content)
+    soup = BeautifulSoup(request.text, 'lxml')
 
-# MitID procedure
-aux = json.loads(base64.b64decode(request.json()["aux"]))
-authorization_code = get_authentication_code(session, aux, method, user_id, password)
-print(f"Your MitID authorization code was ({authorization_code})")
+    request = nordnet_session.post(soup.div.next['data-base-url']+soup.div.next['data-init-auth-path'])
+    if request.status_code != 200:
+        msg = f"\n  Failed Nordnet session.post attempt:\n"\
+              f"    Status code: {request.status_code}\n"\
+              f"    Reason: {request.reason}\n"\
+              f"    Content: {request.content}"
+        raise Exception(msg)
 
-# Second part of Nordnet procedure
-payload = f'''-----------------------------{form_digits}\r\nContent-Disposition: form-data; name="authCode"\r\n\r\n{authorization_code}\r\n-----------------------------{form_digits}--\r\n'''
+    # MitID procedure
+    aux = json.loads(base64.b64decode(request.json()["aux"]))
+    authorization_code = get_authentication_code(nordnet_session, aux, method, user_id, password)
+    print(f"Your MitID authorization code was ({authorization_code})")
 
-headers = {'Content-Type': f'multipart/form-data; boundary=---------------------------{form_digits}'}
-request = session.post(soup.div.next['data-base-url']+soup.div.next['data-auth-code-path'], data=payload, headers=headers)
-request = session.get(soup.div.next['data-base-url']+soup.div.next['data-finalize-auth-path'])
-parsed_url = urlparse(request.url)
-code = parse_qs(parsed_url.query)['code'][0]
+    # Second part of Nordnet procedure
+    payload = f'''-----------------------------{form_digits}\r\nContent-Disposition: form-data; name="authCode"\r\n\r\n{authorization_code}\r\n-----------------------------{form_digits}--\r\n'''
+    headers = {'Content-Type': f'multipart/form-data; boundary=---------------------------{form_digits}'}
 
-payload = {
-    "authenticationProvider": "SIGNICAT",
-    "countryCode":"DK", 
-    "signicat": {
-        "authorizationCode": code,
-        "redirectUri":"https://www.nordnet.dk/login"
+    request = nordnet_session.post(soup.div.next['data-base-url']+soup.div.next['data-auth-code-path'], data=payload, headers=headers)
+    if request.status_code != 200:
+        msg = f"\n  Failed Nordnet session.post attempt:\n"\
+              f"    Status code: {request.status_code}\n"\
+              f"    Reason: {request.reason}\n"\
+              f"    Content: {request.content}"
+        raise Exception(msg)
+
+    request = nordnet_session.get(soup.div.next['data-base-url']+soup.div.next['data-finalize-auth-path'])
+    if request.status_code != 200:
+        msg = f"\n  Failed Nordnet session.get attempt:\n"\
+              f"    Status code: {request.status_code}\n"\
+              f"    Reason: {request.reason}\n"\
+              f"    Content: {request.content}"
+        raise Exception(msg)
+
+    parsed_url = urlparse(request.url)
+    code = parse_qs(parsed_url.query)['code'][0]
+
+    payload = {
+        "authenticationProvider": "SIGNICAT",
+        "countryCode":"DK", 
+        "signicat": {
+            "authorizationCode": code,
+            "redirectUri":"https://www.nordnet.dk/login"
+            }
         }
-    }
 
-session.headers['client-id'] = 'NEXT'
-request = session.post('https://www.nordnet.dk/nnxapi/authentication/v2/sessions', json=payload)
-request = session.post('https://www.nordnet.dk/api/2/authentication/nnx-session/login', json={})
-session.headers['ntag'] = request.headers['ntag']
+    nordnet_session.headers['client-id'] = 'NEXT'
 
-# Get accounts
-#accounts = session.get('https://www.nordnet.dk/api/2/accounts')
-#print(accounts.json())
+    request = nordnet_session.post('https://www.nordnet.dk/nnxapi/authentication/v2/sessions', json=payload)
+    if request.status_code != 200:
+        msg = f"\n  Failed Nordnet session.post attempt:\n"\
+              f"    Status code: {request.status_code}\n"\
+              f"    Reason: {request.reason}\n"\
+              f"    Content: {request.content}"
+        raise Exception(msg)
+    
+    request = nordnet_session.post('https://www.nordnet.dk/api/2/authentication/nnx-session/login', json={})
+    if request.status_code != 200:
+        msg = f"\n  Failed Nordnet session.post attempt:\n"\
+              f"    Status code: {request.status_code}\n"\
+              f"    Reason: {request.reason}\n"\
+              f"    Content: {request.content}"
+        raise Exception(msg)
 
-request = session.post('https://www.nordnet.dk/nnxapi/authorization/v1/tokens', json={})
-bearer_token = request.json()['jwt']
-session.headers['authorization'] = f'Bearer {bearer_token}'
-session.headers['x-locale'] = 'da-DK'
+    nordnet_session.headers['ntag'] = request.headers['ntag']
 
-# Get transactions
-#accids = (',').join([str(account['accid']) for account in accounts.json()])
-#fromdate = '2013-01-01'
-#todate = datetime.strftime(date.today(), '%Y-%m-%d')
+    request = nordnet_session.post('https://www.nordnet.dk/nnxapi/authorization/v1/tokens', json={})
+    if request.status_code not in (200,201):
+        msg = f"\n  Failed Nordnet session.post attempt:\n"\
+              f"    Status code: {request.status_code}\n"\
+              f"    Reason: {request.reason}\n"\
+              f"    Content: {request.content}"
+        raise Exception(msg)
 
-# Get JSON transactions
-# Limited to 800 results, the maximum may be larger
-# Change offset to get subsequent transactions
-# You can get total transactions by requesting: 
-# f'https://api.prod.nntech.io/transaction/transaction-and-notes/v1/transaction-summary?fromDate={fromdate}&toDate={todate}&accids={accids}&includeCancellations=false'
-#transactions_json = session.get(f'https://api.prod.nntech.io/transaction/transaction-and-notes/v1/transactions/page?fromDate={fromdate}&toDate={todate}&accids={accids}&offset=0&limit=800&sort=ACCOUNTING_DATE&sortOrder=DESC&includeCancellations=false')
+    bearer_token = request.json()['jwt']
 
-# Get CSV transactions
-# Tab delimited. To change to semicolon-delimited, add:
-# csv_file = csv_file.replace('\t',';')
-#transactions_csv = session.get(f'https://api.prod.nntech.io/transaction/transaction-and-notes/v1/transactions/csv/filter?accids={accids}&fromDate={fromdate}&toDate={todate}&sort=ACCOUNTING_DATE&sortOrder=DESC&includeCancellations=false')
-#transactions_csv_bytes = transactions_csv.json()['bytes']
-#transactions_csv_decoded_bytes = base64.b64decode(transactions_csv_bytes)
-#csv_file = transactions_csv_decoded_bytes.decode('utf-16')
+    nntech_session = requests.Session()
+    if proxy:
+        nntech_session.proxies.update({"http": f"socks5://{proxy}", "https": f"socks5://{proxy}" })
+    nntech_session.headers['authorization'] = f'Bearer {bearer_token}'
+    nntech_session.headers['x-locale'] = 'da-DK'
+
+    return nordnet_session, nntech_session
+
+if __name__ == "__main__":
+    argparser = get_default_args()
+    args = argparser.parse_args()
+    method, user_id, password, proxy = process_args(args)
+
+    nordnet_session, nntech_session = nordnet_login(user_id, password, method, proxy)
+
+    # Get accounts
+    #accounts = nordnet_session.get('https://www.nordnet.dk/api/2/accounts')
+    #print(accounts.json())
+
+    # Get transactions
+    # accids = (',').join([str(account['accid']) for account in accounts.json()])
+    # fromdate = '2013-01-01'
+    # todate = datetime.strftime(date.today(), '%Y-%m-%d')
+
+    # Get JSON transactions
+    # Limited to 800 results, the maximum may be larger
+    # Change offset to get subsequent transactions
+    # You can get total transactions by requesting: 
+    # f'https://api.prod.nntech.io/transaction/transaction-and-notes/v1/transaction-summary?fromDate={fromdate}&toDate={todate}&accids={accids}&includeCancellations=false'
+    # transactions_json = nntech_session.get(f'https://api.prod.nntech.io/transaction/transaction-and-notes/v1/transactions/page?fromDate={fromdate}&toDate={todate}&accids={accids}&offset=0&limit=800&sort=ACCOUNTING_DATE&sortOrder=DESC&includeCancellations=false')
+    # print(transactions_json.json())
+
+    # Get CSV transactions
+    # Tab delimited. To change to semicolon-delimited, add:
+    # csv_file = csv_file.replace('\t',';')
+    #transactions_csv = nntech_session.get(f'https://api.prod.nntech.io/transaction/transaction-and-notes/v1/transactions/csv/filter?accids={accids}&fromDate={fromdate}&toDate={todate}&sort=ACCOUNTING_DATE&sortOrder=DESC&includeCancellations=false')
+    #transactions_csv_bytes = transactions_csv.json()['bytes']
+    #transactions_csv_decoded_bytes = base64.b64decode(transactions_csv_bytes)
+    #csv_file = transactions_csv_decoded_bytes.decode('utf-16')
+    #print(csv_file)
